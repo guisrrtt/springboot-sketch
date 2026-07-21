754d959d2b0a82f1d4700878468a01688fc5e3ba
Guilherme Serretti <serrettiguilherme@gmail.com>
2026-07-21 19:33:44 -0300

    Project created

---
+++ src/main/java/com/guiserretti/spring_boot_sketch/SpringBootSketchApplication.java
@@ -0,0 +1,13 @@
+package com.guiserretti.spring_boot_sketch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSketchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSketchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/guiserretti/spring_boot_sketch/SpringBootSketchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guiserretti.spring_boot_sketch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSketchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
