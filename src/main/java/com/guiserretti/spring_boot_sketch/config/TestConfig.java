71a17068f6287ff215c887851fa5bde208779f16
Guilherme Serretti <serrettiguilherme@gmail.com>
2026-07-29 08:49:32 -0300

    Payment, one-to-one association

--- src/main/java/com/guiserretti/spring_boot_sketch/config/TestConfig.java
+++ src/main/java/com/guiserretti/spring_boot_sketch/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.guiserretti.spring_boot_sketch.entities.Category;
 import com.guiserretti.spring_boot_sketch.entities.Order;
 import com.guiserretti.spring_boot_sketch.entities.OrderItem;
+import com.guiserretti.spring_boot_sketch.entities.Payment;
 import com.guiserretti.spring_boot_sketch.entities.Product;
 import com.guiserretti.spring_boot_sketch.entities.User;
 import com.guiserretti.spring_boot_sketch.entities.enums.OrderStatus;
@@ -81,5 +82,10 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 }
